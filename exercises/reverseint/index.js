7fa95df1f9134bedd79408d6a0681610bc43e624
Abdul Khan <akhan17@outlook.com>
2021-04-21 22:49:00 -0500

    reverseint solved

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,38 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+    let str;
+    let reverse = [];
+
+    let absval = Math.abs(n);
+    let absvalstr = absval.toString();
+
+    if(Math.sign(n) === -1){
+        
+        for(let i = absvalstr.length-1; i >=0; i-- ){
+            reverse.push(absvalstr[i]);
+        }
+        
+        let revJoined = parseInt(reverse.join("")) *-1;
+
+        return revJoined
+        
+        
+    }else{
+
+        for(let i = absvalstr.length-1; i >=0; i-- ){
+            reverse.push(absvalstr[i]);
+        }
+
+        let revJoined = parseInt(reverse.join(""));
+        return revJoined
+
+    }
+
+}
+
+reverseInt(1223)
 
 module.exports = reverseInt;
