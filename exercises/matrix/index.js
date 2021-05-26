af4206e71fe6d19d8308bc82754dda55afb95aab
Abdul Khan <akhan17@outlook.com>
2021-05-26 17:08:47 -0500

    --

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -16,53 +16,54 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
-   let results = []
+   
+    let results = []
 
-   for(let i = 0 ; i < n; i++){
-    results.push([])
-   }
+    for(let i = 0; i < n; i++){
+        results.push([])
+    }
 
-   let counter = 1;
+    let counter = 1
 
-   let startCol = 0 
-   let endCol = n-1
+    let startRow = 0 
+    let endRow = n-1
 
-   let startRow = 0
-   let endRow = n-1
+    let startCol = 0
+    let endCol = n-1
 
-   while(startCol <= endCol && startRow <= endRow){
-        //Top
-        for(let i = startCol; i <= endCol; i++ ){
-            results[startRow][i] = counter;
+    while(startCol <= endCol && startRow <= endRow){
+        //top 
+        for(let i = startCol; i <= endCol; i++){
+            results[startRow][i] = counter 
             counter++
         }
         startRow++
 
-        //Right
+        //right
         for(let i = startRow; i <= endRow; i++){
             results[i][endCol] = counter
             counter++
         }
         endCol--
 
-        //Bottom
-        for(let i = endCol; i>=startCol; i--){
+        //bottom 
+        for(let i = endCol; i >= startCol; i--){
             results[endRow][i] = counter;
             counter++
         }
         endRow--
 
-        //wrapping
+        //left curving
         for(let i = endRow; i >= startRow; i--){
-            results[i][startCol] = counter
+            results[i][startCol]= counter
             counter++
         }
         startCol++
-   }
+
+    }
 
     console.log(results)
-    return results 
-   
+    return results
 }
 
 matrix(4)
