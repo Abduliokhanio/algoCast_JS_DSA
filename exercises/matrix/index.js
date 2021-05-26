0d513dd35bff4dcd11ef03260a09031e8893376f
Abdul Khan <akhan17@outlook.com>
2021-05-26 17:11:24 -0500

    --

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -17,53 +17,7 @@
 
 function matrix(n) {
    
-    let results = []
-
-    for(let i = 0; i < n; i++){
-        results.push([])
-    }
-
-    let counter = 1
-
-    let startRow = 0 
-    let endRow = n-1
-
-    let startCol = 0
-    let endCol = n-1
-
-    while(startCol <= endCol && startRow <= endRow){
-        //top 
-        for(let i = startCol; i <= endCol; i++){
-            results[startRow][i] = counter 
-            counter++
-        }
-        startRow++
-
-        //right
-        for(let i = startRow; i <= endRow; i++){
-            results[i][endCol] = counter
-            counter++
-        }
-        endCol--
-
-        //bottom 
-        for(let i = endCol; i >= startCol; i--){
-            results[endRow][i] = counter;
-            counter++
-        }
-        endRow--
-
-        //left curving
-        for(let i = endRow; i >= startRow; i--){
-            results[i][startCol]= counter
-            counter++
-        }
-        startCol++
-
-    }
-
-    console.log(results)
-    return results
+    
 }
 
 matrix(4)
