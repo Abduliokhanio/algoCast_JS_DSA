dd75ca4285a43e52720111a094375e5203dd0e74
Abdul Khan <akhan17@outlook.com>
2021-06-10 14:45:17 -0500

    completed queue

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,24 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+
+    constructor(){
+        this.data = []
+    }
+
+    push(n){
+        this.data.push(n)
+    }
+
+    pop(){
+        return this.data.pop(this.data[0])
+    }
+
+    peek(){
+        return this.data[0]
+    }
+
+}
 
 module.exports = Stack;
