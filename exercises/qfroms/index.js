8b62b88210039764b375f108fc84c67651b45dc4
Abdul Khan <akhan17@outlook.com>
2021-06-10 15:53:12 -0500

    completed qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,40 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+
+    constructor(){
+        this.stackA = new Stack();
+        this.stackB = new Stack();
+    }  
+
+    add(n){
+        this.stackA.push(n)
+    }
+
+    peek(){
+        while(this.stackA.peek()){
+            this.stackB.push(this.stackA.pop())
+        }
+        let rv = this.stackB.peek()
+        while(this.stackB.peek()){
+            this.stackA.push(this.stackB.pop())
+        }
+        return rv
+    }
+
+    remove(){
+        while(this.stackA.peek()){
+            this.stackB.push(this.stackA.pop())
+        }
+        let rv = this.stackB.pop()
+        while(this.stackB.peek()){
+            this.stackA.push(this.stackB.pop())
+        }
+        return rv
+
+    }
+
+}
 
 module.exports = Queue;
